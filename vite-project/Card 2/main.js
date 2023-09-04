034f1927eb20b4accc968d362caf3d6ea58e4b21
santi <santi@sanferar.com>
2023-09-04 12:17:23 +0200

    Diseño de Card 2 y 3

---
+++ vite-project/Card 2/main.js
@@ -0,0 +1,14 @@
+const app = document.getElementById('app');
+
+
+const botonLey = document.getElementById('__tooltyp');
+const botonCerrar = document.getElementById('cerrar');
+const collapse = document.getElementById('collapse');
+
+botonLey.addEventListener('click', ()=>{
+    collapse.classList.add('contenedor__historial__collapse--opacity');
+})
+
+botonCerrar.addEventListener('click', ()=>{
+    collapse.classList.remove('contenedor__historial__collapse--opacity');
+})
